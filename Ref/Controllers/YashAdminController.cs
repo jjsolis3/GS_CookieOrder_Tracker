64dc4008203aa664001b46dd16a37f7689f84237
Jose J Solis <jjsolis3@asu.edu>
2026-02-02 17:47:28 -0800

    updated reference theme files which i would like to use with this app

---
+++ Ref/Controllers/YashAdminController.cs
@@ -0,0 +1,356 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace YashAdmin.Controllers
+{
+    public class YashAdminController : Controller
+    {
+        public IActionResult AddBlog()
+        {
+            return View();
+        }
+		public IActionResult AddEmail()
+		{
+			return View();
+		}
+		public IActionResult AddUser()
+		{
+			return View();
+		}
+		public IActionResult Analytics()
+		{
+			return View();
+		}
+		public IActionResult AppCalender()
+		{
+			return View();
+		}
+		public IActionResult AppProfile1()
+		{
+			return View();
+		}
+		public IActionResult AppProfile2()
+		{
+			return View();
+		}
+		public IActionResult Blog()
+		{
+			return View();
+		}
+		public IActionResult BlogCategory()
+		{
+			return View();
+		}
+		public IActionResult ChartChartist()
+		{
+			return View();
+		}
+		public IActionResult ChartChartjs()
+		{
+			return View();
+		}
+		public IActionResult ChartFlot()
+		{
+			return View();
+		}
+		public IActionResult ChartMorris()
+		{
+			return View();
+		}
+		public IActionResult ChartPeity()
+		{
+			return View();
+		}
+		public IActionResult ChartSparkline()
+		{
+			return View();
+		}
+		public IActionResult Chat()
+		{
+			return View();
+		}
+		public IActionResult CMSBlog()
+		{
+			return View();
+		}
+		public IActionResult Content()
+		{
+			return View();
+		}
+		public IActionResult ContentAdd()
+		{
+			return View();
+		}
+		public IActionResult CRM()
+		{
+			return View();
+		}
+		public IActionResult EcomCheckout()
+		{
+			return View();
+		}
+		public IActionResult EcomCustomers()
+		{
+			return View();
+		}
+		public IActionResult EcomInvoice()
+		{
+			return View();
+		}
+		public IActionResult EcomProductDetail()
+		{
+			return View();
+		}
+		public IActionResult EcomProductGrid()
+		{
+			return View();
+		}
+		public IActionResult EcomProductList()
+		{
+			return View();
+		}
+		public IActionResult EcomProductOrder()
+		{
+			return View();
+		}
+		public IActionResult EditProfile()
+		{
+			return View();
+		}
+		public IActionResult EmailCompose()
+		{
+			return View();
+		}
+		public IActionResult EmailInbox()
+		{
+			return View();
+		}
+		public IActionResult EmailRead()
+		{
+			return View();
+		}
+		public IActionResult EmailTemplate()
+		{
+			return View();
+		}
+		public IActionResult EmptyPage()
+		{
+			return View();
+		}
+		public IActionResult FormCkeditor()
+		{
+			return View();
+		}
+		public IActionResult FormElement()
+		{
+			return View();
+		}
+		public IActionResult FormPickers()
+		{
+			return View();
+		}
+		public IActionResult FormValidation()
+		{
+			return View();
+		}
+		public IActionResult FormWizard()
+		{
+			return View();
+		}
+		public IActionResult Index()
+		{
+			return View();
+		}
+		public IActionResult Index2()
+		{
+			return View();
+		}
+		public IActionResult Index3()
+		{
+			return View();
+		}
+		public IActionResult Index4()
+		{
+			return View();
+		}
+		public IActionResult Index5()
+		{
+			return View();
+		}
+		public IActionResult MapJqvmap()
+		{
+			return View();
+		}
+		public IActionResult Menu()
+		{
+			return View();
+		}
+		public IActionResult MyProject()
+		{
+			return View();
+		}
+		public IActionResult Notifications()
+		{
+			return View();
+		}
+		public IActionResult PageError400()
+		{
+			return View();
+		}
+		public IActionResult PageError403()
+		{
+			return View();
+		}
+		public IActionResult PageError404()
+		{
+			return View();
+		}
+		public IActionResult PageError500()
+		{
+			return View();
+		}
+		public IActionResult PageError503()
+		{
+			return View();
+		}
+		public IActionResult PageForgotPassword()
+		{
+			return View();
+		}
+		public IActionResult PageLockScreen()
+		{
+			return View();
+		}
+		public IActionResult PageLogin()
+		{
+			return View();
+		}
+		public IActionResult PageRegister()
+		{
+			return View();
+		}
+		public IActionResult PostDetails()
+		{
+			return View();
+		}
+		public IActionResult Products()
+		{
+			return View();
+		}
+		public IActionResult Project()
+		{
+			return View();
+		}
+		public IActionResult Sales()
+		{
+			return View();
+		}
+		public IActionResult TableBootstrapBasic()
+		{
+			return View();
+		}
+		public IActionResult TableDatatableBasic()
+		{
+			return View();
+		}
+		public IActionResult Task()
+		{
+			return View();
+		}
+		public IActionResult UCLightgallery()
+		{
+			return View();
+		}
+		public IActionResult UCNestable()
+		{
+			return View();
+		}
+		public IActionResult UCNouiSlider()
+		{
+			return View();
+		}
+		public IActionResult UCSelect2()
+		{
+			return View();
+		}
+		public IActionResult UCSweetalert()
+		{
+			return View();
+		}
+		public IActionResult UCToastr()
+		{
+			return View();
+		}
+		public IActionResult UIAccordion()
+		{
+			return View();
+		}
+		public IActionResult UIAlert()
+		{
+			return View();
+		}
+		public IActionResult UIBadge()
+		{
+			return View();
+		}
+		public IActionResult UIButton()
+		{
+			return View();
+		}
+		public IActionResult UIButtonGroup()
+		{
+			return View();
+		}
+		public IActionResult UICard()
+		{
+			return View();
+		}
+		public IActionResult UICarousel()
+		{
+			return View();
+		}
+		public IActionResult UIDropdown()
+		{
+			return View();
+		}
+		public IActionResult UIGrid()
+		{
+			return View();
+		}
+		public IActionResult UIListGroup()
+		{
+			return View();
+		}
+		public IActionResult UIModal()
+		{
+			return View();
+		}
+		public IActionResult UIPagination()
+		{
+			return View();
+		}
+		public IActionResult UIPopover()
+		{
+			return View();
+		}
+		public IActionResult UIProgressbar()
+		{
+			return View();
+		}
+		public IActionResult UITab()
+		{
+			return View();
+		}
+		public IActionResult UITypography()
+		{
+			return View();
+		}
+		public IActionResult UserList()
+		{
+			return View();
+		}
+		public IActionResult WidgetBasic()
+		{
+			return View();
+		}
+	}
+}
